87069401f9be707cfc1a39cdf322c3760fd27ca7
harishreddyhrd <hari.000703@gmail.com>
2021-10-16 12:41:45 +0530

    REDUCER - implemented

---
+++ src/app/counter/store/counter.reducer.ts
@@ -0,0 +1,32 @@
+import { createReducer, on } from '@ngrx/store';
+import {
+  decrementAction,
+  incrementAction,
+  resetAction,
+} from './counter.actions';
+
+const _counterReducer = createReducer(
+  initialState,
+  on(incrementAction, (state) => {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  }),
+  on(decrementAction, (state) => {
+    return {
+      ...state,
+      count: state.count - 1,
+    };
+  }),
+  on(resetAction, (state) => {
+    return {
+      ...state,
+      count: 0,
+    };
+  })
+);
+
+export function theCounterReducer(state, action) {
+  return _counterReducer(state, action);
+}
