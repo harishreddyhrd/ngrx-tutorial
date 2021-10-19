3909065b3c811aa57c51107434cce335dbffe2bb
harishreddyhrd <hari.000703@gmail.com>
2021-10-19 15:30:33 +0530

    modified store name

--- src/app/counter/store/counter.selectors.ts
+++ src/app/counter/store/counter.selectors.ts
@@ -2,7 +2,7 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { CounterState } from './counter.state';
 
 const counterStateFeatureSelector =
-  createFeatureSelector<CounterState>('count');
+  createFeatureSelector<CounterState>('counter'); //same as that in StoreModule.forRoot({ counter: theCounterReducer }) in app.module.ts
 
 export const countSelector = createSelector(
   counterStateFeatureSelector,
