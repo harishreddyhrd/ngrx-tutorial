b0467dc40db8b0b9cc76216dd4fc317671ba079b
harishreddyhrd <hari.000703@gmail.com>
2021-10-21 08:32:25 +0530

    minor change

--- src/app/store/posts-state/posts.state.ts
+++ src/app/store/posts-state/posts.state.ts
@@ -1,11 +1,11 @@
 import { Post } from 'src/app/models/post.model';
 
 export interface PostsState {
-  posts: Post[];
+  postsList: Post[];
 }
 
 export const initialPostsState: PostsState = {
-  posts: [
+  postsList: [
     { id: 1, title: 'POST-01_TITLE', description: 'POST-01_DESCRIPTION' },
     { id: 2, title: 'POST-02_TITLE', description: 'POST-02_DESCRIPTION' },
     { id: 3, title: 'POST-03_TITLE', description: 'POST-03_DESCRIPTION' },
