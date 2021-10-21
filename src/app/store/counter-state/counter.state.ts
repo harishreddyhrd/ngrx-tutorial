c9e94b8dd4410cfd511916e30b8299a58b4829bd
harishreddyhrd <hari.000703@gmail.com>
2021-10-21 08:24:21 +0530

    Posts STATE implemented

--- src/app/store/counter-state/counter.state.ts
+++ src/app/store/counter-state/counter.state.ts
@@ -3,7 +3,7 @@ export interface CounterState {
   counterType: string;
 }
 
-export const initialState = {
+export const initialState: CounterState = {
   count: 13,
   counterType: 'Baloon Counter',
 };
---
+++ src/app/store/posts-state/posts.state.ts
@@ -0,0 +1,13 @@
+import { Post } from 'src/app/models/post.model';
+
+export interface PostsState {
+  posts: Post[];
+}
+
+export const initialState: PostsState = {
+  posts: [
+    { id: 1, title: 'POST-01_TITLE', description: 'POST-01_DESCRIPTION' },
+    { id: 2, title: 'POST-02_TITLE', description: 'POST-02_DESCRIPTION' },
+    { id: 3, title: 'POST-03_TITLE', description: 'POST-03_DESCRIPTION' },
+  ],
+};
