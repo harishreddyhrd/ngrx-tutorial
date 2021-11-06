5563e8be80abb04c75cf912c16c321b9c23bbb00
harishreddyhrd <hari.000703@gmail.com>
2021-11-06 06:06:01 +0530

    auth login component generated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CounterComponent } from './counter/counter.component';
 import { HomeComponent } from './home/home.component';
-import { AddPostComponent } from './posts/add-post/add-post.component';
-import { EditPostComponent } from './posts/edit-post/edit-post.component';
-import { PostsListComponent } from './posts/posts-list/posts-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -21,6 +17,13 @@ const routes: Routes = [
     loadChildren: () =>
       import('./posts/posts.module').then(
         (incomingData) => incomingData.PostsModule
+      ),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.module').then(
+        (incomingData) => incomingData.AuthModule
       ),
   },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment'; // Angular CLI environment
-import { AppReducer } from './store/app.reducer';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     StoreModule.forRoot({}),
     // Instrumentation must be imported after importing StoreModule (config is optional)
     StoreDevtoolsModule.instrument({
