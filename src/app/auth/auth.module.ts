5722f6ea4c88d106e7e94b601fd41c1d09148d44
harishreddyhrd <hari.000703@gmail.com>
2021-11-08 04:39:34 +0530

    auth login created

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, ReactiveFormsModule, AuthRoutingModule],
 })
 export class AuthModule {}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showErrorsForEmailInput() {
+    const emailPlaceHolder = this.loginForm.get('email');
+    if (emailPlaceHolder?.touched && !emailPlaceHolder.valid) {
+      if (emailPlaceHolder.errors.required) {
+        return 'Email is required';
+      } else if (emailPlaceHolder.errors.email) {
+        return 'Invalid email pattern';
+      }
+    }
   }
 
+  showErrorsForPasswordInput() {
+    const passwordPlaceholder = this.loginForm.get('password');
+    if (passwordPlaceholder?.touched && !passwordPlaceholder.valid) {
+      if (passwordPlaceholder.errors.required) {
+        return 'Password is required';
+      } else if (passwordPlaceholder.errors.minlength) {
+        return 'Password should have atleast 8 characters';
+      }
+    }
+  }
+
+  onLogin() {
+    console.log(this.loginForm.value);
+  }
 }
